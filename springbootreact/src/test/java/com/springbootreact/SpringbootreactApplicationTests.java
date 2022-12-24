db1e61b393f30e5fee97abdc94ece57b79987214
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-12-23 22:10:28 -0300

    FEAT: Implementando a entidade Product e testando o endpoint

---
+++ springbootreact/src/test/java/com/springbootreact/SpringbootreactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
